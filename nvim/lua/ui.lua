e6ef177a7080b529e2db0175844c2d78254a4226
Rub3cK0r3 <rubecode11001@gmail.com>
2026-01-25 22:29:51 +0100

    fix bug

---
+++ nvim/lua/ui.lua
@@ -0,0 +1,17 @@
+-- Basic UI
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.swapfile = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.termguicolors = true
+vim.o.background = "dark"
+vim.cmd("colorscheme tokyonight")
+
+vim.cmd("hi Normal guibg=#010101 guifg=#C5C8C6")
+vim.cmd("hi NormalNC guibg=#292b2e")
+vim.o.background = "dark"
+
+
