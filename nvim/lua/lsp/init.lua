709c8dcb95e5b812cb5f67ebf8992de8c3c192cd
RubenMurciaeduca <2854159@alu.murciaeduca.es>
2026-02-15 17:53:48 +0100

    fix lsp init.lua deprectated

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,23 +1,21 @@
 -- lsp main config file
 --
-local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local servers = require("lsp.lsp_servers").servers
 
 for name, cfg in pairs(servers) do
-  local filetypes = cfg.filetypes or {name}
-
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = filetypes,
-    callback = function()
-      if not lspconfig[name].manager.active then
-        lspconfig[name].setup(vim.tbl_deep_extend("force", {
-          capabilities = capabilities,
-        }, cfg))
-      end
-    end,
-    desc = "Lazy-load LSP: " .. name,
-  })
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = cfg.filetypes or {name},
+        callback = function()
+            if not vim.lsp.get_active_clients({ name = name })[1] then
+                vim.lsp.start(vim.tbl_deep_extend("force", {
+                    name = name,
+                    capabilities = capabilities,
+                }, cfg))
+            end
+        end,
+        desc = "Lazy-load LSP: " .. name,
+    })
 end
 
 require("lsp.jdtls")
