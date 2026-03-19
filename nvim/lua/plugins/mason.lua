72d1c4f547f425f39244b8b1c53fcf65641c7a34
Rub3ck0r3 <rubecode11001@gmail.com>
2026-03-19 17:24:37 +0100

    "chore(lsp): integrate mason-lspconfig and ensure LSP servers are auto-installed"

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -3,30 +3,22 @@ return {
     "williamboman/mason.nvim",
     lazy = false,
     config = function()
-      local ok, mason = pcall(require, "mason")
-      if not ok then
-        vim.notify("Error cargando mason.nvim", vim.log.levels.ERROR)
-        return
-      end
-
-      mason.setup({
-        ui = {
-          border = "rounded",
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "bashls","clangd","cssls","dockerls","gopls","html",
+          "intelephense","jdtls","jsonls","lua_ls","pyright",
+          "rust_analyzer","sqlls","yamlls"
         },
+        automatic_installation = true,
       })
-
-      local ok_lsp, mason_lspconfig = pcall(require, "mason-lspconfig")
-      if ok_lsp then
-        mason_lspconfig.setup({
-          ensure_installed = {},
-          automatic_installation = false,
-        })
-      end
     end,
   },
 }
