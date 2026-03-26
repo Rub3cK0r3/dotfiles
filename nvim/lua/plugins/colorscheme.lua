69a6f5c0655759d4d0cf97d378c377b1e6de6f13
Rub3ck0r3 <rubecode11001@gmail.com>
2026-03-26 23:06:16 +0100

    chore(nvim): switch colorscheme and update UI highlights

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,4 @@
+return {
+  { "KKPMW/sacredforest-vim", name = "sacredforest", priority = 1000 },
+}
+
--- nvim/lua/ui.lua
+++ nvim/lua/ui.lua
@@ -8,11 +8,20 @@ vim.o.swapfile = false
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.cursorline = true        
+vim.opt.signcolumn = "no"      
 
 vim.opt.termguicolors = true
 vim.o.background = "dark"
 
 -- Colorscheme
-vim.cmd("colorscheme catppuccin")
+vim.cmd("colorscheme sacredforest")
+vim.api.nvim_set_hl(0, "Normal", { fg="#F5EBD9", bg="#2B303B" })  
+vim.api.nvim_set_hl(0, "Comment", { fg="#B0B0B0", italic=true })  
+vim.api.nvim_set_hl(0, "Function", { fg="#88C0D0", bold=true })  
+vim.api.nvim_set_hl(0, "Keyword", { fg="#EBCB8B", bold=true })  
+vim.api.nvim_set_hl(0, "String", { fg="#A3BE8C", bold=false })  
+vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE", fg="#969696" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "NONE", fg = "#EBCB8B", bold=true })  
 
 return M
