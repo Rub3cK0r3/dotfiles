2288d32dc193332b3eba7d152d7c0b6b10b79a7c
Rub3cK0r3 <rubecode11001@gmail.com>
2026-04-03 00:59:13 +0200

    chore(nvim): created custom initial dashboard and added feature to ui.lua

--- nvim/lua/ui.lua
+++ nvim/lua/ui.lua
@@ -41,4 +41,66 @@ require('smear_cursor').setup({
     hide_cursor = true,
 })
 
+-- rub3ck0r3's custom initial nvim dashboard, really cool btw.
+require("dashboard").setup({
+  theme = "doom",
+  config = {
+    header = {
+      "",
+      ".--.",
+      "|x_x |",
+      "|:_/ |",
+      "//   \\ \\",
+      "(|     | )",
+      "/'\\_   _/`\\",
+      "\\___)=(___/",
+      "",
+      ":: NVIM ::",
+      ":: access granted ::",
+      "",
+      "󱩊  @rub3ck0r3",
+      "",
+    },
+    center = {
+      {
+        icon = "󱀹 ",
+        desc = "New File",
+        action = "enew",
+        key = "n",
+      },
+      {
+        icon = "󰩉 ",
+        desc = "Search File",
+        action = "Telescope find_files",
+        key = "f",
+      },
+      {
+        icon = "  ",
+        desc = "Edit Config",
+        action = "edit ~/.config/nvim/init.lua",
+        key = "c",
+      },
+      {
+        icon = "󰒲  ",
+        desc = "Update Plugins",
+        action = "Lazy update",
+        key = "u",
+      },
+      {
+        icon = "  ",
+        desc = "Recent Files",
+        action = "Telescope oldfiles",
+        key = "r",
+      },
+      {
+        icon = "󰈆 ",
+        desc = "Exit",
+        action = "qa",
+        key = "q",
+      },
+    },
+  },
+})
+
+
 return M
