c649528643d222492a3fb81d99e84a14ab280a3f
Rub3cK0r3 <rubecode11001@gmail.com>
2026-04-02 15:54:05 +0200

    full config refactor: I made up a better config based on dracula + kanagawa theme

--- nvim/lua/ui.lua
+++ nvim/lua/ui.lua
@@ -17,4 +17,17 @@ vim.o.showmode = false
 
 vim.cmd("colorscheme kanagawa")
 
+local groups = {
+  "Normal", "NormalNC", "NormalFloat", "StatusLine", "StatusLineNC", "VertSplit",
+}
+
+for _, group in ipairs(groups) do
+  vim.api.nvim_set_hl(0, group, { bg = "none" })
+end
+
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NvimTreeVertSplit", { bg = "none" })
+
 return M
