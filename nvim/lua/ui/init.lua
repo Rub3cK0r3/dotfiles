f0f3e1d6e755c388a0231658f99d6f69d9e0b61f
Rub3ck0r3 <rubecode11001@gmail.com>
2026-04-07 11:20:47 +0200

    chore(nvim ui): nvim improved ui entrypoint logic

--- nvim/lua/ui/init.lua
+++ nvim/lua/ui/init.lua
@@ -1,14 +1,8 @@
 -- UI entrypoint - rub3ck0r3
-local modules = {
-  "basic",
-  "misc",
-  "transparency",
-  "smear_cursor",
-  "dashboard",
-  "notes"
-}
 
-for _, m in ipairs(modules) do
-  require("ui." .. m)
-end
+-- critical first
+require("ui.basic")
+
+-- everything else auto
+require("core.loader").load_all("ui")
 
