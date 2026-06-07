1ea74f97f81f82eff03df1d69240a084c2d45ee0
Rub3ck0r3 <rubecode11001@gmail.com>
2026-06-07 10:17:24 +0200

    feat(nvim/plugins): Added copilot autocompletion features

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+        },
+      },
+      panel = {
+        enabled = false,
+      },
+    },
+  },
+}
